ce194d9e7e1eaa056cea7b0b5adcbca2f3888758
Neyleth <neylethquintero1997@gmail.com>
2024-12-10 22:27:39 -0300

    adding weather app

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ menuIcon.onclick = () => {
   menuIcon.classList.toggle("fa-x");
   navbar.classList.toggle("active");
 };
+function toggleText(event) {
+  event.preventDefault(); // Prevenir el comportamiento predeterminado del enlace
+  const moreText = event.target.previousElementSibling; // Obtiene el span con el texto adicional
+  const readMore = event.target; // Obtiene el enlace "Leer más"
+
+  if (moreText.style.display === "none") {
+    moreText.style.display = "inline"; // Muestra el texto adicional
+    readMore.textContent = "Leer menos"; // Cambia el texto del enlace a "Leer menos"
+  } else {
+    moreText.style.display = "none"; // Oculta el texto adicional
+    readMore.textContent = "Leer más"; // Cambia el texto del enlace a "Leer más"
+  }
+}
